a51e0960bbfced0cb714199b7553ffd43bd705b9
KeyLo99 <keylo99official@gmail.com>
2019-08-25 00:21:08 +0200

    Add RequestTask class for the intent response handling

--- app/src/main/java/com/k3/dialogflowbot/MainActivity.java
+++ app/src/main/java/com/k3/dialogflowbot/MainActivity.java
@@ -1,12 +1,16 @@
 package com.k3.dialogflowbot;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2beta1.DetectIntentRequest;
+import com.google.cloud.dialogflow.v2beta1.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2beta1.QueryInput;
 import com.google.cloud.dialogflow.v2beta1.SessionName;
 import com.google.cloud.dialogflow.v2beta1.SessionsClient;
 import com.google.cloud.dialogflow.v2beta1.SessionsSettings;
@@ -47,6 +51,40 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
         return false;
+    }
+
+    private static class RequestTask extends AsyncTask<Void, Void, DetectIntentResponse> {
+
+        private SessionName session;
+        private SessionsClient sessionsClient;
+        private QueryInput queryInput;
+
+        private RequestTask(SessionName session,
+                          SessionsClient sessionsClient, QueryInput queryInput) {
+            this.session = session;
+            this.sessionsClient = sessionsClient;
+            this.queryInput = queryInput;
+        }
+
+        @Override
+        protected DetectIntentResponse doInBackground(Void... voids) {
+            try {
+                DetectIntentRequest detectIntentRequest =
+                        DetectIntentRequest.newBuilder()
+                                .setSession(session.toString())
+                                .setQueryInput(queryInput)
+                                .build();
+                return sessionsClient.detectIntent(detectIntentRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(DetectIntentResponse response) {
+
+        }
     }
 
 }
